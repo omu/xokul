25689e9e9b9f30e3ded78d57ff300f21f00d99e7
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-07-13 16:13:58 +0300

    response: remove leftover attributes

--- app/services/response.rb
+++ app/services/response.rb
@@ -1,17 +1,15 @@
 # frozen_string_literal: true
 
-# WIP
 module Services
   class Response
-    delegate :http_fault?, :soap_fault?, :successful?, :to_hash, :to_json,
-             to: :@soap_object
+    delegate :body, :http_fault?, :soap_fault?, :successful?, :to_hash,
+             :to_json, to: :@object
 
-    def initialize(soap_object)
-      @soap_object = soap_object
-      @body        = soap_object.body
-      @status      = soap_object.status
+    def initialize(object)
+      @object = object
     end
 
-    attr_reader :body, :status
+    # In future
+    def mapper; end
   end
 end
