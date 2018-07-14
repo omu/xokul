8f74d08ca29618f5dd9b13c941b9265534a363a0
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-07-15 00:11:32 +0300

    Create references controller

---
+++ app/controllers/yoksis/references_controller.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module Yoksis
+  class ReferencesController < ApplicationController
+    before_action :set_references
+
+    # Test
+    def district
+      response = @references.district(params[:province_code])
+      render(
+        json: response.collection(:ilce_kodlari),
+        each_serializer: Yoksis::References::DistrictSerializer
+      )
+    end
+
+    private
+
+    def set_references
+      @references = references_klass.new
+    end
+
+    def references_klass
+      "Services::Yoksis::#{version}::References".constantize
+    end
+
+    def version
+      Services::Yoksis::VERSION.upcase
+    end
+  end
+end
---
+++ test/controllers/yoksis/references_controller_test.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'test_helper'
+
+module Yoksis
+  class ReferencesControllerTest < ActionDispatch::IntegrationTest
+    # test "the truth" do
+    #   assert true
+    # end
+  end
+end
