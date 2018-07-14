78639f2b976060f5b729437a1dc41c4bdf25f28a
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-07-15 01:41:46 +0300

    osym: completed

---
+++ app/services/osym/v1/osym.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+module Services
+  module Osym
+    module V1
+      class Examination
+        WSDL_URL = 'https://vps.osym.gov.tr/Ext/Provider/BilgiServisi/Sonuc?wsdl'
+
+        def initialize(username, password)
+          @client = Client.new(WSDL_URL)
+          @client.basic_auth username, password
+          @client.wsse_auth username, password
+        end
+
+        def groups
+          client.call(:sinav_grup_bilgilerini_getir)
+        end
+
+        def result(tck_no, result_id)
+          client.call(
+            :sinav_sonuc_xml, adayTcKimlikNo: tck_no, sonucId: result_id
+          )
+        end
+
+        def results(tck_no, year, group_id)
+          client.call(
+            :sinav_sonuclari_getir,
+            adayTcKimlikNo: tck_no, yil: year, grupId: group_id
+          )
+        end
+
+        attr_reader :client
+      end
+    end
+  end
+end
---
+++ app/services/osym/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module Services
+  module Osym
+    VERSION = 'v1'
+  end
+end
