89fc746f18a9b67ccb3abdce2823d4a7f964c449
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-07-16 15:26:16 +0300

    Add KPS Public service

---
+++ app/services/kps/public/v1/verify.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Services
+  module Kps
+    module Public
+      module V1
+        class Verify
+          WSDL_URL = 'https://tckimlik.nvi.gov.tr/Service/KPSPublic.asmx?WSDL'
+
+          def initialize
+            @client = Client.new(WSDL_URL)
+          end
+
+          def identity_number(tck_no, year_of_birth, firstname, lastname)
+            client.call(
+              :tc_kimlik_no_dogrula,
+              TCKimlikNo: tck_no, Ad: firstname, Soyad: lastname,
+              DogumYili: year_of_birth
+            )
+          end
+
+          attr_reader :client
+        end
+      end
+    end
+  end
+end
