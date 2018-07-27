593e5897a8d48932ac8900167e2ea9656f097eda
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-07-27 13:39:43 +0300

    kps/private: remove erb function from utils

--- app/services/kps/private/utils.rb
+++ app/services/kps/private/utils.rb
@@ -8,15 +8,11 @@ module Services
         private_constant :TIME_FORMAT
 
         def self.created_time
-          Time.now.utc.strftime(TIME_FORMAT)
+          Time.zone.now.strftime(TIME_FORMAT)
         end
 
         def self.expires_time(duration = 600)
-          (Time.now + duration).utc.strftime(TIME_FORMAT)
-        end
-
-        def self.erb(file)
-          ERB.new(File.read(file)).result(binding)
+          (Time.zone.now + duration).strftime(TIME_FORMAT)
         end
 
         def self.http_get(url, header: {}, body: nil)
