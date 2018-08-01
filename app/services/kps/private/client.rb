ba0f56ddf893695f3bac718d8458eec5c2ee794c
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-08-01 14:53:49 +0300

    kps/private: Add client file

---
+++ app/services/kps/private/client.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'securerandom'
+
+module Services
+  module Kps
+    module Private
+      class Client
+        def initialize(wst_attributes: {}, sts_attributes: {}); end
+      end
+    end
+  end
+end
