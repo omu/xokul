bbc27edfa835296908f42125eb5bb20dcc056b0f
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-08-02 15:53:14 +0300

    Add a few error class

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,34 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  rescue_from ActionController::ParameterMissing, with: :parameter_missing
-  rescue_from Services::HTTPError,             with: :services_error
-  rescue_from Services::SOAPFault,             with: :services_error
-  rescue_from Services::UnknownOperationError, with: :services_error
-  rescue_from Services::TCPError,              with: :services_error
+  rescue_from ActionDispatch::ParamsParser::ParseError, with: :bad_request
+  rescue_from ActionController::BadRequest,             with: :bad_request
+  rescue_from Rack::Utils::ParameterTypeError,          with: :bad_request
+  rescue_from ActionController::ParameterMissing,       with: :bad_request
+  rescue_from Rack::Utils::InvalidParameterError,       with: :bad_request
+  rescue_from ActionController::RoutingError,           with: :not_found
+  rescue_from AbstractController::ActionNotFound,       with: :not_found
+  rescue_from ActionController::UnknownFormat,          with: :not_acceptable
+  rescue_from ActionController::NotImplemented,         with: :not_implemented
+  rescue_from Services::HTTPError,                      with: :services_error
+  rescue_from Services::SOAPFault,                      with: :services_error
+  rescue_from Services::UnknownOperationError,          with: :services_error
+  rescue_from Services::TCPError,                       with: :services_error
 
-  def parameter_missing(exception)
+  def bad_request(exception)
     render_json message: exception, status: :bad_request
+  end
+
+  def not_acceptable(exception)
+    render_json message: exception, status: :not_acceptable
+  end
+
+  def not_found(exception)
+    render_json message: exception, status: :bad_request
+  end
+
+  def not_implemented(exception)
+    render_json message: exception, status: :not_implemented
   end
 
   def services_error(exception)
