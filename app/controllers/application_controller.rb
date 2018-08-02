839bce6e6728812c91d4aa609771002d1edc1c8a
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-08-02 21:12:50 +0300

    Fix style

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  rescue_from ActionController::BadRequest,             with: :bad_request
-  rescue_from Rack::Utils::ParameterTypeError,          with: :bad_request
-  rescue_from ActionController::ParameterMissing,       with: :bad_request
-  rescue_from Rack::Utils::InvalidParameterError,       with: :bad_request
-  rescue_from ActionController::RoutingError,           with: :not_found
-  rescue_from AbstractController::ActionNotFound,       with: :not_found
-  rescue_from ActionController::UnknownFormat,          with: :not_acceptable
-  rescue_from ActionController::NotImplemented,         with: :not_implemented
-  rescue_from Services::HTTPError,                      with: :services_error
-  rescue_from Services::SOAPError,                      with: :services_error
-  rescue_from Services::UnknownOperationError,          with: :services_error
-  rescue_from Services::TCPError,                       with: :services_error
+  rescue_from ActionController::BadRequest,       with: :bad_request
+  rescue_from Rack::Utils::ParameterTypeError,    with: :bad_request
+  rescue_from ActionController::ParameterMissing, with: :bad_request
+  rescue_from Rack::Utils::InvalidParameterError, with: :bad_request
+  rescue_from ActionController::RoutingError,     with: :not_found
+  rescue_from AbstractController::ActionNotFound, with: :not_found
+  rescue_from ActionController::UnknownFormat,    with: :not_acceptable
+  rescue_from ActionController::NotImplemented,   with: :not_implemented
+  rescue_from Services::HTTPError,                with: :services_error
+  rescue_from Services::SOAPError,                with: :services_error
+  rescue_from Services::UnknownOperationError,    with: :services_error
+  rescue_from Services::TCPError,                 with: :services_error
 
   def bad_request(exception)
     render_json message: exception, status: :bad_request
