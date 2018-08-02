b4ec2706f21361042e254b3bb7f04d56bcb37f7e
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-08-02 10:59:23 +0300

    kps/private: WIP: client

--- app/services/kps/private/client.rb
+++ app/services/kps/private/client.rb
@@ -4,7 +4,24 @@ module Services
   module Kps
     module Private
       class Client
-        def initialize(wst_attributes: {}, sts_attributes: {}); end
+        def initialize(**params)
+          @endpoint  = params[:endpoint]
+          @reference = params[:reference]
+          @sts = Sts.new(
+            username:  params[:username],
+            password:  params[:password],
+            endpoint:  @endpoint,
+            reference: @reference
+          )
+        end
+
+        def call(action, **arguments)
+          Wst.new(
+            endpoint:  @endpoint,
+            reference: @reference,
+            token:     @sts.token_request
+          ).request(action, arguments)
+        end
       end
     end
   end
