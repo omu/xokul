50e57fca4da7919c7ba366f6c9441497f5347a9b
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-08-07 13:24:04 +0300

    Update references controller

--- app/controllers/yoksis/references_controller.rb
+++ app/controllers/yoksis/references_controller.rb
@@ -5,11 +5,12 @@ module Yoksis
     before_action :set_references
 
     include ActionsResource
+    include YoksisResource
 
     Services::Yoksis.module::REFERENCES_METHODS.each_key do |method|
       define_method(method) do
         render(
-          json: @references.send(method).absolute_data,
+          json: @references.send(method).absolute,
           each_serializer: serializer(method)
         )
       end
@@ -18,7 +19,7 @@ module Yoksis
     def district
       response = @references.district(params.require(:province_code))
       render(
-        json: response.absolute_data,
+        json: response.absolute,
         each_serializer: serializer(:district)
       )
     end
@@ -31,10 +32,6 @@ module Yoksis
 
     def serializer(method)
       "Yoksis::References::#{method.camelize}Serializer".constantize
-    end
-
-    def api_version
-      Services::Yoksis::VERSION
     end
   end
 end
