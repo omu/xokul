2db1cdcea15a0c46f2368cdafc169b0cd0acd16f
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-08-08 12:34:38 +0300

    Fix stylistic issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,26 +1,26 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  rescue_from ActionController::BadRequest,             with: :bad_request
-  rescue_from ActionController::ParameterMissing,       with: :bad_request
-  rescue_from ActionController::RoutingError,           with: :not_found
+  rescue_from ActionController::BadRequest,           with: :bad_request
+  rescue_from ActionController::ParameterMissing,     with: :bad_request
+  rescue_from ActionController::RoutingError,         with: :not_found
   rescue_from Support::Client::HTTPError,             with: :services_error
   rescue_from Support::Client::SOAPError,             with: :services_error
   rescue_from Support::Client::UnknownOperationError, with: :services_error
   rescue_from Support::Client::TCPError,              with: :services_error
 
   def bad_request(exception)
-    render json:   { identifier: 'api_bad_request', message: exception },
+    render json: { identifier: 'api_bad_request', message: exception },
            status: :bad_request
   end
 
   def not_found(exception)
-    render json:   { identifier: 'api_not_found', message: exception },
+    render json: { identifier: 'api_not_found', message: exception },
            status: :not_found
   end
 
   def services_error(exception)
-    render json:   { identifier: exception.identifier, message: exception },
+    render json: { identifier: exception.identifier, message: exception },
            status: exception.code
   end
 end
