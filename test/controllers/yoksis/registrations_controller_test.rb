c0c93bb6926b9de818859193180243803c09afe4
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-08-09 12:15:10 +0300

    Fix rubocop offense

--- test/controllers/yoksis/registrations_controller_test.rb
+++ test/controllers/yoksis/registrations_controller_test.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 require 'test_helper'
 
-class Yoksis::RegistrationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+module Yoksis
+  class RegistrationsControllerTest < ActionDispatch::IntegrationTest
+    # test "the truth" do
+    #   assert true
+    # end
+  end
 end
