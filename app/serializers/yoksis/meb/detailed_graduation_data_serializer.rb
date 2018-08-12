2a874a4e8f575c74c08c9a9b8e1303c5ee0d783e
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-08-12 15:44:57 +0300

    Update meb serializer's attributes

--- app/serializers/yoksis/meb/detailed_graduation_data_serializer.rb
+++ app/serializers/yoksis/meb/detailed_graduation_data_serializer.rb
@@ -3,26 +3,26 @@
 module Yoksis
   module Meb
     class DetailedGraduationDataSerializer < ActiveModel::Serializer
-      attribute(:id_number)          { object[:tckimlikno]       }
-      attribute(:first_name)         { object[:adi]              }
-      attribute(:last_name)          { object[:soyadi]           }
-      attribute(:school_id)          { object[:okulkodu]         }
-      attribute(:school_name)        { object[:okuladi]          }
-      attribute(:okul_alan_kodu)     { object[:okulalankodu]     }
-      attribute(:okul_alan_adi)      { object[:okulalanadi]      }
-      attribute(:okul_dal_kodu)      { object[:okuldalkodu]      }
-      attribute(:okul_dal_adi)       { object[:okuldaladi]       }
-      attribute(:okul_birincisi)     { object[:okulbirincisi]    }
-      attribute(:okul_il_adi)        { object[:okuliladi]        }
-      attribute(:okul_il_kodu)       { object[:okulilkodu]       }
-      attribute(:okul_ilce_adi)      { object[:okulilceadi]      }
-      attribute(:okul_ilce_kodu)     { object[:okulilcekodu]     }
-      attribute(:ogrenim_turu)       { object[:ogrenimturu]      }
-      attribute(:mezun_durumu_kodu)  { object[:mezundurumukodu]  }
-      attribute(:mezun_durumu)       { object[:mezundurumu]      }
-      attribute(:mezuniyet_tarih)    { object[:mezuniyettarih]   }
-      attribute(:not_sistemi)        { object[:notsistemi]       }
-      attribute(:diploma_notu_puani) { object[:diplomanotupuani] }
+      attribute(:id_number)           { object[:tckimlikno]       }
+      attribute(:first_name)          { object[:adi]              }
+      attribute(:last_name)           { object[:soyadi]           }
+      attribute(:school_id)           { object[:okulkodu]         }
+      attribute(:school_name)         { object[:okuladi]          }
+      attribute(:school_field_code)   { object[:okulalankodu]     }
+      attribute(:school_field_name)   { object[:okulalanadi]      }
+      attribute(:school_branch_code)  { object[:okuldalkodu]      }
+      attribute(:school_branch_name)  { object[:okuldaladi]       }
+      attribute(:top_scoring_student) { object[:okulbirincisi]    }
+      attribute(:city_name)           { object[:okuliladi]        }
+      attribute(:city_code)           { object[:okulilkodu]       }
+      attribute(:district_name)       { object[:okulilceadi]      }
+      attribute(:district_code)       { object[:okulilcekodu]     }
+      attribute(:instruction_type)    { object[:ogrenimturu]      }
+      attribute(:grad_status_code)    { object[:mezundurumukodu]  }
+      attribute(:grad_status)         { object[:mezundurumu]      }
+      attribute(:graduation_date)     { object[:mezuniyettarih]   }
+      attribute(:grading_system)      { object[:notsistemi]       }
+      attribute(:diploma_grade)       { object[:diplomanotupuani] }
     end
   end
 end
