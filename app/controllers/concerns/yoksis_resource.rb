3cc1aad10a9cf959e04781ddeda3b5f3014e0136
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-08-12 15:24:04 +0300

    Add helper to serialize actions

--- app/controllers/concerns/yoksis_resource.rb
+++ app/controllers/concerns/yoksis_resource.rb
@@ -3,6 +3,10 @@
 module YoksisResource
   extend ActiveSupport::Concern
 
+  def action_serializer(action = nil)
+    "Yoksis::#{controller_name.camelize}::#{(action || action_name).camelize}Serializer".constantize
+  end
+
   def api_version
     Services::Yoksis::VERSION
   end
