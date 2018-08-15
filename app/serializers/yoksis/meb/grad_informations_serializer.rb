0bd9c3a3da2096044ffb10dc42e459d53a7be839
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-08-15 12:48:43 +0300

    Convert attribute types in serializers

--- app/serializers/yoksis/meb/grad_informations_serializer.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Yoksis
-  module Meb
-    class GradInformationsSerializer < ActiveModel::Serializer
-      attribute(:status) { object[:aciklama] }
-    end
-  end
-end
