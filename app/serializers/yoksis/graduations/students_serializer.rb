aec88e627e4f1f819eb3f56435d87831f4aa38eb
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-08-17 15:12:19 +0300

    Fix rubocop offenses

--- app/serializers/yoksis/graduations/students_serializer.rb
+++ app/serializers/yoksis/graduations/students_serializer.rb
@@ -2,15 +2,14 @@
 
 module Yoksis
   module Graduations
-    # rubocop:disable Metrics/LineLength
     class StudentsSerializer < Serializer
       attribute(:id_number)              { object[:tckno].try(:to_i) }
       attribute(:first_name)             { object[:adi]              }
       attribute(:last_name)              { object[:soyadi]           }
       attribute(:father_name)            { object[:baba_adi]         }
       attribute(:mother_name)            { object[:anne_adi]         }
-      attribute(:date_of_birth)          { build_date(*object[:dogum_tarihi].values_at(:yil, :ay, :gun)     }
-      attribute(:date_of_graduation)     { build_date(*object[:mezuniyet_tarihi].values_at(:yil, :ay, :gun) }
+      attribute(:date_of_birth)          { build_date(*object[:dogum_tarihi].values_at(:yil, :ay, :gun))     }
+      attribute(:date_of_graduation)     { build_date(*object[:mezuniyet_tarihi].values_at(:yil, :ay, :gun)) }
       attribute(:diploma_grade)          { object[:diploma_notu].try(:to_i)        }
       attribute(:diploma_grading_system) { object[:diploma_not_sistemi].try(:to_i) }
       attribute(:diploma_no)             { object[:diploma_no]                     }
@@ -20,6 +19,5 @@ module Yoksis
       attribute(:program_name)           { object[:program_adi]                    }
       attribute(:unit_id)                { object[:birim_id].try(:to_i)            }
     end
-    # rubocop:enable Metrics/LineLength
   end
 end
