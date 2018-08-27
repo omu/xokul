c1aeaf25077b76c49a3e2e28b3e70e1bce50310f
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-08-27 12:56:59 +0300

    Minor

--- app/services/yoksis/yoksis.rb
+++ app/services/yoksis/yoksis.rb
@@ -6,10 +6,11 @@ module Services
   class Yoksis
     require_relative "lib/#{VERSION}/base"
     require_relative "lib/#{VERSION}/constants"
-    require_relative "lib/#{VERSION}/graduations"
+    require_relative "lib/#{VERSION}/graduates"
     require_relative "lib/#{VERSION}/meb"
     require_relative "lib/#{VERSION}/references"
     require_relative "lib/#{VERSION}/registrations"
+    require_relative "lib/#{VERSION}/resumes"
     require_relative "lib/#{VERSION}/staff"
     require_relative "lib/#{VERSION}/students"
     require_relative "lib/#{VERSION}/units"
