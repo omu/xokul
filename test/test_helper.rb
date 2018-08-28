7e76099f7de36d552faeeb5fbe19af683461dfc9
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-08-28 12:41:04 +0300

    Add test paths to test_helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,10 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+Dir[Rails.root.join('test', 'support', '**', '*.rb')].each do |file|
+  require file
+end
+
 module ActiveSupport
   class TestCase
     fixtures :all
