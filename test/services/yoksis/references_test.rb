1c67a6dfb9d9a377fd08b808113eac725771e109
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-09-12 17:25:44 +0300

    Add references service tests

--- test/services/yoksis/references_test.rb
+++ test/services/yoksis/references_test.rb
@@ -5,8 +5,33 @@ require 'test_helper'
 module Yoksis
   class References < ActiveSupport::TestCase
     setup do
-      VCR.use_cassette('yoksis_references_cassette') do
-        @client = Client.new(Services::Yoksis.module_path::References::WSDL_URL)
+      @references = Services::Yoksis::References.new
+    end
+
+    test 'Check response of administrative_units' do
+      VCR.use_cassette('yoksis/references/administrative_units_cassette') do
+        @references.administrative_units.each do |unit|
+          assert unit.key?(:birim_id)
+          assert unit.key?(:birim_adi)
+          assert unit.key?(:bagli_oldugu_birim_id)
+        end
+      end
+    end
+
+    test 'Check response of districts' do
+      VCR.use_cassette('yoksis/references/districts_cassette') do
+        @references.districts(city_code: 33).each do |district|
+          assert_kod_ad district
+        end
+      end
+    end
+
+    Services::Yoksis::References::METHOD_VARIABLES.each_key do |method|
+      next if %i[districts administrative_units].include?(method)
+      test "Check response of #{method}" do
+        VCR.use_cassette("yoksis/references/#{method}_cassette") do
+          @references.send(method).each { |city| assert_kod_ad city }
+        end
       end
     end
   end
