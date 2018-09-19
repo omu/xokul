b97429c513597637a9d877369d35196417255620
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-09-19 14:55:22 +0300

    Improve error handling

--- lib/client/client/response.rb
+++ lib/client/client/response.rb
@@ -3,17 +3,13 @@
 class Response
   delegate :header, :body, to: :@soap_response
 
-  attr_accessor :status_body
-
   def initialize(soap_response)
     @soap_response = soap_response
   end
 
-  def successful?
-    return @soap_response.successful? unless status_body&.is_a?(Array)
-    return true if status_body.empty?
-    digged = body.dig(*status_body)
-    return false if digged.present? || /[Ee]rror|[Ff]ail|[Ha]ta/.match?(digged)
-    true
+  def dig(*args)
+    dug = body.dig(*args)
+    return dug unless block_given?
+    yield dug
   end
 end
