61bcf8cd861cca6b4103e3a0e971754942b1f602
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-09-19 14:58:16 +0300

    Fix error classes in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,8 @@ class ApplicationController < ActionController::API
   rescue_from Client::SOAPError,                  with: :services_error
   rescue_from Client::TCPError,                   with: :services_error
   rescue_from Client::UnknownOperationError,      with: :services_error
-  rescue_from Client::InvalidResponseError,       with: :services_error
+  rescue_from Services::InvalidResponseError,     with: :services_error
+  rescue_from Services::NoContentError,           with: :services_error
 
   def bad_request(exception)
     render json: { status: 400, error: exception }, status: :bad_request
@@ -24,11 +25,7 @@ class ApplicationController < ActionController::API
   end
 
   def render_as_json(data)
-    if data
-      serializer_type = data.is_a?(Array) ? :each_serializer : :serializer
-      render json: data, "#{serializer_type}": action_serializer
-    else
-      render json: data, status: :no_content
-    end
+    serializer_type = data.is_a?(Array) ? :each_serializer : :serializer
+    render json: data, "#{serializer_type}": action_serializer
   end
 end
