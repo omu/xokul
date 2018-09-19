3ae66330fcffc278ae9e08d2c07003a072e9e308
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-09-19 14:59:45 +0300

    Refactor the all yoksis serializers

--- app/serializers/yoksis/resumes/projects_serializer.rb
+++ app/serializers/yoksis/resumes/projects_serializer.rb
@@ -7,24 +7,24 @@ module Yoksis
       attribute(:name)                   { object[:proje_ad]                                }
       attribute(:subject)                { object[:proje_konusu]                            }
       attribute(:status_id)              { object[:proje_durumu_id].safe_to_i               }
-      attribute(:status_text)            { object[:proje_durumu_ad]                         }
-      attribute(:updated_at)             { Time.zone.parse object[:guncelleme_tarihi] || '' }
+      attribute(:status_name)            { object[:proje_durumu_ad]                         }
+      attribute(:date_of_update)         { Time.zone.parse object[:guncelleme_tarihi] || '' }
       attribute(:budget)                 { object[:butce].safe_to_i                         }
       attribute(:location_id)            { object[:proje_konumu_id].safe_to_i               }
-      attribute(:location_text)          { object[:proje_konumu_ad]                         }
+      attribute(:location_name)          { object[:proje_konumu_ad]                         }
       attribute(:type_id)                { object[:proje_turu_id].safe_to_i                 }
-      attribute(:type_text)              { object[:proje_turu_ad]                           }
+      attribute(:type_name)              { object[:proje_turu_ad]                           }
       attribute(:currency_id)            { object[:para_birimi_id].safe_to_i                }
-      attribute(:currency_text)          { object[:para_birimi_ad]                          }
+      attribute(:currency_name)          { object[:para_birimi_ad]                          }
       attribute(:active_or_passive_id)   { object[:aktif_pasif].safe_to_i                   }
-      attribute(:active_or_passive_text) { object[:aktif_pasif_ad]                          }
-      attribute(:purview_id)             { object[:kapsam].safe_to_i                        }
-      attribute(:purview_text)           { object[:kapsam_ad]                               }
-      attribute(:academic_title_id)      { object[:unvan_id].safe_to_i                      }
-      attribute(:academic_title_text)    { object[:unvan_ad]                                }
+      attribute(:active_or_passive_name) { object[:aktif_pasif_ad]                          }
+      attribute(:scope_id)               { object[:kapsam].safe_to_i                        }
+      attribute(:scope_name)             { object[:kapsam_ad]                               }
+      attribute(:title_id)               { object[:unvan_id].safe_to_i                      }
+      attribute(:title_name)             { object[:unvan_ad]                                }
       attribute(:university_id)          { object[:kurum_id].safe_to_i                      }
       attribute(:university_name)        { object[:kurum_ad]                                }
-      attribute(:tesv_point)             { object[:tesv_puan].safe_to_f                     }
+      attribute(:incentive_point)        { object[:tesv_puan].safe_to_f                     }
 
       attribute :started_at do
         next unless object[:bas_tar]
