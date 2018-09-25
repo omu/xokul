d71b521bf57c863301083f93766c1d365338d6ae
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-09-25 15:56:42 +0300

    Improve error handling

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,22 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
-  rescue_from ActionController::BadRequest,       with: :bad_request
-  rescue_from ActionController::ParameterMissing, with: :bad_request
-  rescue_from ActionController::RoutingError,     with: :not_found
-  rescue_from Client::HTTPError,                  with: :services_error
-  rescue_from Client::SOAPError,                  with: :services_error
-  rescue_from Client::TCPError,                   with: :services_error
-  rescue_from Client::UnknownOperationError,      with: :services_error
-  rescue_from Services::InvalidResponseError,     with: :services_error
-  rescue_from Services::NoContentError,           with: :services_error
+  rescue_from Client::HTTPError,              with: :services_error
+  rescue_from Client::SOAPError,              with: :services_error
+  rescue_from Client::TCPError,               with: :services_error
+  rescue_from Client::UnknownOperationError,  with: :services_error
+  rescue_from Services::InvalidResponseError, with: :services_error
+  rescue_from Services::NoContentError,       with: :services_error
 
-  def bad_request(exception)
-    render json: { status: 400, error: exception }, status: :bad_request
+  rescue_from ActionController::ParameterMissing do |exception|
+    render json: {
+      status: 400, error: "Missing parameters: #{exception.param}"
+    }, status: :bad_request
   end
 
-  def not_found(exception)
-    render json: { status: 404, error: exception }, status: :not_found
+  def not_found
+    render json: { status: 404, error: 'Not found.' }, status: :not_found
   end
 
   def services_error(exception)
