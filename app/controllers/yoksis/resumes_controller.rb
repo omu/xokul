7601c238215f863da4f290ac33403cf1480f4ac7
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-09-26 23:52:42 +0300

    Add awards action to resumes controller

--- app/controllers/yoksis/resumes_controller.rb
+++ app/controllers/yoksis/resumes_controller.rb
@@ -18,6 +18,7 @@ module Yoksis
 
     alias academic_duties       articles
     alias administrative_duties articles
+    alias awards                articles
     alias books                 articles
     alias certifications        articles
     alias editorships           articles
