16fe0bd15aadaec3b8ed4beb1538d2b987fbb60f
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-09-26 19:06:35 +0300

    Add foreign_languages serializer

---
+++ app/serializers/yoksis/resumes/foreign_languages_serializer.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+module Yoksis
+  module Resumes
+    class ForeignLanguagesSerializer < Serializer
+      attribute(:id)                     { object[:y_id].safe_to_i                                                   }
+      attribute(:language_id)            { object[:dil_id].safe_to_i                                                 }
+      attribute(:language_name)          { object[:dil_ad].titleize_tr                                               }
+      attribute(:language_testing_id)    { object[:dil_sinav_id].safe_to_i                                           }
+      attribute(:language_testing_name)  { object[:dil_sinav_ad].upcase_tr                                           }
+      attribute(:language_testing_point) { object[:puan].safe_to_i                                                   }
+      attribute(:equivalent_point)       { object[:esdegerpuan].safe_to_i                                            }
+      attribute(:year)                   { object[:yil].safe_to_i                                                    }
+      attribute(:term_id)                { object[:donem_id].safe_to_i                                               }
+      attribute(:term_name)              { object[:donem_ad].titleize_tr                                             }
+      attribute(:active_or_passive_id)   { object[:aktif_pasif].safe_to_i                                            }
+      attribute(:active_or_passive_name) { object[:aktif_pasif_ad].titleize_tr                                       }
+      attribute(:date_of_update)         { object[:guncelleme_tarihi] && Time.zone.parse(object[:guncelleme_tarihi]) }
+    end
+  end
+end
