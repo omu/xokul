e77258b866817e280fa4828c5e25f46a04a944bc
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-12-11 11:35:16 +0300

    Remove leftover code

--- app/services/lib/services/detsis.rb
+++ app/services/lib/services/detsis.rb
@@ -1,19 +1,5 @@
 # frozen_string_literal: true
 
-# Load high-priority files before endpoint classes
-require_relative 'yoksis/base'
-require_relative 'yoksis/constants'
-
-# Load endpoint classes
-require_relative 'yoksis/endpoints/graduates'
-require_relative 'yoksis/endpoints/meb'
-require_relative 'yoksis/endpoints/military'
-require_relative 'yoksis/endpoints/references'
-require_relative 'yoksis/endpoints/resumes'
-require_relative 'yoksis/endpoints/staff'
-require_relative 'yoksis/endpoints/students'
-require_relative 'yoksis/endpoints/units'
-
 module Services
   class Detsis
     ENDPOINTS = [].freeze
