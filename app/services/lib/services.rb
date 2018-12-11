89dbf88437ca511992a1f859c3d441501d59a126
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-12-11 11:30:40 +0300

    Add DETSIS service

--- app/services/lib/services.rb
+++ app/services/lib/services.rb
@@ -2,12 +2,13 @@
 
 require_relative 'services/errors'
 require_relative 'services/yoksis'
+require_relative 'services/detsis'
 require_relative 'services/kps'
 require_relative 'services/osym'
 require_relative 'services/version'
 
 module Services
-  AVAILABLE_SERVICES = { version: VERSION, services: %i[osym kps yoksis] }.freeze
+  AVAILABLE_SERVICES = { version: VERSION, services: %i[detsis osym kps yoksis] }.freeze
 
   def self.service_endpoints(service)
     const_get "#{service}::ENDPOINTS"
