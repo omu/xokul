f44b6b6fe0c6074e36e3efab61c60c58979f2141
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-12-18 13:58:16 +0300

    Requre and configure webmock for minitest in test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,6 +4,9 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+require 'webmock/minitest'
+WebMock.disable_net_connect!(allow: %w[api.codacy.com], allow_localhost: true)
+
 module ActiveSupport
   class TestCase
     fixtures :all
