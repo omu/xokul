c1eb47be3548cd8d14c3f0b2ce05828ef0e32ecb
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-12-18 10:38:15 +0300

    Update test helper

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,17 +4,8 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
-Dir[Rails.root.join('test', 'support', '**', '*.rb')].each do |file|
-  require file
-end
-
 module ActiveSupport
   class TestCase
     fixtures :all
-
-    def assert_kod_ad(object)
-      assert object.key?(:kod)
-      assert object.key?(:ad)
-    end
   end
 end
