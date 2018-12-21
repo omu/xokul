9e2e12dd0eafb107514feee30818e36165159517
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-12-21 14:38:50 +0300

    Add simple inflection for Turkish language

--- config/initializers/inflections.rb
+++ config/initializers/inflections.rb
@@ -1,18 +1,14 @@
 # frozen_string_literal: true
 
-# Be sure to restart your server when you modify this file.
+ActiveSupport::Inflector.inflections(:tr) do |inflect|
+  inflect.clear
 
-# Add new inflection rules using the following format. Inflections
-# are locale specific, and you may define rules for as many different
-# locales as you wish. All of these examples are active by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.plural /^(ox)$/i, '\1en'
-#   inflect.singular /^(ox)en/i, '\1'
-#   inflect.irregular 'person', 'people'
-#   inflect.uncountable %w( fish sheep )
-# end
+  inflect.plural(/([aoıu][^aoıueöiü]{,6})$/, '\1lar')
+  inflect.plural(/([eöiü][^aoıueöiü]{,6})$/, '\1ler')
 
-# These inflection rules are supported but not enabled by default:
-# ActiveSupport::Inflector.inflections(:en) do |inflect|
-#   inflect.acronym 'RESTful'
-# end
+  inflect.singular(/l[ae]r$/i, '')
+
+  inflect.irregular('ben', 'biz')
+  inflect.irregular('sen', 'siz')
+  inflect.irregular('o', 'onlar')
+end
--- lib/support/turkish_support.rb
+++ lib/support/turkish_support.rb
@@ -4,17 +4,4 @@ require_relative 'turkish_support/refinery'
 
 module TurkishSupport
   include Refinery
-
-  Inflection = proc do |inflect|
-    inflect.clear
-
-    inflect.plural(/([aoıu][^aoıueöiü]{,6})$/, '\1lar')
-    inflect.plural(/([eöiü][^aoıueöiü]{,6})$/, '\1ler')
-
-    inflect.singular(/l[ae]r$/i, '')
-
-    inflect.irregular('ben', 'biz')
-    inflect.irregular('sen', 'siz')
-    inflect.irregular('o', 'onlar')
-  end
 end
