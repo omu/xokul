e7b8ea83daa965434b697ee827317b58e3339ce0
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-12-21 14:33:30 +0300

    Add Turkish support

---
+++ lib/support/turkish_support.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+require_relative 'turkish_support/constants'
+require_relative 'turkish_support/refinery'
+
+module TurkishSupport
+  include Refinery
+
+  Inflection = proc do |inflect|
+    inflect.clear
+
+    inflect.plural(/([aoıu][^aoıueöiü]{,6})$/, '\1lar')
+    inflect.plural(/([eöiü][^aoıueöiü]{,6})$/, '\1ler')
+
+    inflect.singular(/l[ae]r$/i, '')
+
+    inflect.irregular('ben', 'biz')
+    inflect.irregular('sen', 'siz')
+    inflect.irregular('o', 'onlar')
+  end
+end
