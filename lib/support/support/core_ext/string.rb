c729fa3c89443ecc939eda390fad12c384ff94a6
Hüseyin Tekinaslan <htkaslan@gmail.com>
2018-12-25 11:55:19 +0300

    Refactor titleize_turkish

--- lib/support/support/core_ext/string.rb
+++ lib/support/support/core_ext/string.rb
@@ -1,16 +1,24 @@
 # frozen_string_literal: true
 
 class String
-  TURKISH_CONJUNCTIONS = %w[
-    ama ancak dahi fakat ile lakin ve veya yani
+  COMMON_ABBREVIATIONS = %w[
+    ab abd abitur act ales aselsan aihm baum cmk cmuk dgs gata gce khk kktc kpds kpss les meb myo omü sat tbmm tck
+    todaie toefl trt tsk tübitak uzem yds ydus ygs yök yös ömss öss ösym ösys öyp üds
   ].freeze
-  private_constant :TURKISH_CONJUNCTIONS
+
+  TURKISH_CONJUNCTIONS = %w[
+    ve veya veyahut oysa oysaki ki ise ile hem hele fakat çünkü ama
+  ].freeze
 
   def titleize_turkish
     downcase(:turkic).split.map do |word|
-      next word if TURKISH_CONJUNCTIONS.include?(word)
-
-      word.capitalize(:turkic)
-    end.join ' '
+      if COMMON_ABBREVIATIONS.include? word
+        word.upcase(:turkic)
+      elsif TURKISH_CONJUNCTIONS.include? word
+        word
+      else
+        word.capitalize(:turkic)
+      end
+    end.join(' ')
   end
 end
