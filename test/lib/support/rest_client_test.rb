7310ce0f97f6c23f7045754eacbd8dafc861ee9e
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-01-29 21:40:18 +0300

    .

--- test/lib/support/rest_client_test.rb
+++ test/lib/support/rest_client_test.rb
@@ -4,14 +4,21 @@ require 'test_helper'
 
 class RestClientTest < ActiveSupport::TestCase
   setup do
+    # Base URL.
     @insecure_url = 'http://example.com'
     @secure_url   = 'https://example.com'
+    
+    # URLs derived from the insecure base URL.
+    @body_url    = @insecure_url + '/body'
+    @error_url   = @insecure_url + '/error'
+    @header_url  = @insecure_url + '/header'
+    @rest_url    = @insecure_url + '/rest'
+    @timeout_url = @insecure_url + '/timeout'
   end
 
   test 'makes sure that supported HTTP methods work correctly' do
     RestClient::SUPPORTED_HTTP_METHODS.each do |method|
       stub_request(method, @insecure_url)
-
       assert RestClient.send(method, @insecure_url)
     end
   end
@@ -24,24 +31,17 @@ class RestClientTest < ActiveSupport::TestCase
 
   test 'makes sure that the :ok? works correctly' do
     stub_request(:get, @insecure_url)
-
     assert RestClient.get(@insecure_url).ok?
   end
 
   test 'makes sure that the :code works correctly' do
-    fatal_error_url = @insecure_url + '/fatal_error'
-
-    stub_request(:get, fatal_error_url).to_return(status: 500)
-
-    assert_equal RestClient.get(fatal_error_url).code, 500
+    stub_request(:get, @error_url).to_return(status: 500)
+    assert_equal RestClient.get(@error_url).code, 500
   end
 
   test 'makes sure that the :decode works correctly' do
-    json_response_url = @insecure_url + '/json_response'
-
-    stub_request(:get, json_response_url).to_return(body: '{ "foo": "bar" }')
-
-    assert_equal RestClient.get(json_response_url).decode, 'foo' => 'bar'
+    stub_request(:get, @rest_url).to_return(body: '{ "foo": "bar" }')
+    assert_equal RestClient.get(@rest_url).decode, 'foo' => 'bar'
   end
 
   test 'raises error if called with invalid HTTP option' do
@@ -52,35 +52,25 @@ class RestClientTest < ActiveSupport::TestCase
 
   test 'makes sure that it works correctly with SSL' do
     stub_request(:post, @secure_url)
-
     assert RestClient.post(
       @secure_url, use_ssl: true, verify_mode: OpenSSL::SSL::VERIFY_PEER
     )
   end
 
   test 'makes sure that timeout works correctly' do
-    timeout_url = @insecure_url + '/timeout'
-
-    stub_request(:get, timeout_url).to_timeout
-
+    stub_request(:get, @timeout_url).to_timeout
     assert_raise Net::OpenTimeout do
-      RestClient.get(timeout_url, open_timeout: 1)
+      RestClient.get(@timeout_url, open_timeout: 1)
     end
   end
 
   test 'makes sure that it works correctly with header' do
-    header_url = @insecure_url + '/headers'
-
-    stub_request(:get, header_url).with(headers: { foo: 'bar' })
-
-    assert RestClient.get(header_url, header: { foo: 'bar' })
+    stub_request(:get, @header_url).with(headers: { foo: 'bar' })
+    assert RestClient.get(@header_url, header: { foo: 'bar' })
   end
 
   test 'makes sure that it works correctly with body' do
-    body_url = @insecure_url + '/body'
-
-    stub_request(:post, body_url).with(body: 'foo')
-
-    assert RestClient.post(body_url, payload: 'foo')
+    stub_request(:post, @body_url).with(body: 'foo')
+    assert RestClient.post(@body_url, payload: 'foo')
   end
 end
