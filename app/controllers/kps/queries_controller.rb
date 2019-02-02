4df6ab04264a8587a8b78540ebf5e64d1a5835f9
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-02-03 00:14:39 +0300

    Fix minor issue

--- app/controllers/kps/queries_controller.rb
+++ app/controllers/kps/queries_controller.rb
@@ -2,20 +2,26 @@
 
 module Kps
   class QueriesController < ApplicationController
+    before_action :set_queries
+
     include ActionsResource
 
     def addresses
-      render_as_json Services::Kps::Queries.addresses(id_number: secure_params.require(:id_number))
+      render_as_json @queries.addresses(id_number: secure_params.require(:id_number))
     end
 
     def identities
-      render_as_json Services::Kps::Queries.identities(id_number: secure_params.require(:id_number))
+      render_as_json @queries.identities(id_number: secure_params.require(:id_number))
     end
 
     private
 
     def secure_params
       params.require(:query).permit(:id_number)
+    end
+
+    def set_queries
+      @queries = Services::Kps::Queries.new
     end
   end
 end
