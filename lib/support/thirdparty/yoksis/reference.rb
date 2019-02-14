d8fde7e4cbcb1cc00a483c9f736f2980aa5bfee0
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-02-14 15:57:08 +0300

    Add missing methods to references

--- lib/support/thirdparty/yoksis/reference.rb
+++ lib/support/thirdparty/yoksis/reference.rb
@@ -32,6 +32,82 @@ module Thirdparty
         client.request(:get_ilce_getir, message: { ILKODU: city_code })
       end
 
+      def entrance_types
+        client.request(:get_ilce_getir)
+      end
+
+      def gender
+        client.request(:get_mernis_cinsiyet)
+      end
+
+      def martyrs_relatives
+        client.request(:get_ogrenci_gazi_sehit_yakini)
+      end
+
+      def nationalities
+        client.request(:get_mernis_uyruk)
+      end
+
+      def staff_titles
+        client.request(:get_kadro_gorev_unvan)
+      end
+
+      def student_disability_types
+        client.request(:get_ogrenci_engel_turu)
+      end
+
+      def student_dropout_types
+        client.request(:get_ogrenci_ayrilma_nedeni)
+      end
+
+      def student_education_levels
+        client.request(:get_ogrenci_doykm)
+      end
+
+      def student_entrance_point_types
+        client.request(:get_ogrenci_giris_puan_turu)
+      end
+
+      def student_entrance_types
+        client.request(:get_ogrenci_giris_turu)
+      end
+
+      def student_grades
+        client.request(:get_ogrenci_sinif)
+      end
+
+      def student_grading_systems
+        client.request(:get_ogrenci_diploma_not_sistemi)
+      end
+
+      def student_punishment_types
+        client.request(:get_ceza_turu)
+      end
+
+      def student_studentship_rights
+        client.request(:get_ogrenci_ogrencilik_hakki)
+      end
+
+      def unit_instruction_languages
+        client.request(:get_ogrenim_dili)
+      end
+
+      def unit_instruction_types
+        client.request(:get_ogrenim_turu)
+      end
+
+      def unit_statuses
+        client.request(:get_aktiflik_durumu)
+      end
+
+      def unit_types
+        client.request(:get_birim_turu)
+      end
+
+      def university_types
+        client.request(:get_universite_turu)
+      end
+
       protected
 
       attr_reader :client
