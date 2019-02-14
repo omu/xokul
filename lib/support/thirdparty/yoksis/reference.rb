f35ffb5af51aac24ad3ecf880231f803f8769d32
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-02-14 15:00:04 +0300

    .

--- lib/support/thirdparty/yoksis/reference.rb
+++ lib/support/thirdparty/yoksis/reference.rb
@@ -12,8 +12,24 @@ module Thirdparty
         @client = SoapClient.new(endpoint)
       end
 
+      def administrative_functions
+        client.request(:get_personel_gorev)
+      end
+
+      def administrative_units
+        client.request(:get_idari_birimler)
+      end
+
+      def countries
+        client.request(:get_mernis_ulke)
+      end
+
       def cities
         client.request(:get_il_getir)
+      end
+
+      def districts(city_code)
+        client.request(:get_ilce_getir, message: { ILKODU: city_code })
       end
 
       protected
