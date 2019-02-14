5e1a2deadaad0a268f5d615b2235487c415a40da
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-02-14 11:51:09 +0300

    Add Yoksis service files

---
+++ lib/support/yoksis.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative 'yoksis/graduate'
+require_relative 'yoksis/meb'
+require_relative 'yoksis/reference'
+require_relative 'yoksis/resume'
+require_relative 'yoksis/staff'
+require_relative 'yoksis/unit'
