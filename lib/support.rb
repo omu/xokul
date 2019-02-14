6268917706acfa5ad82217bf01297380459477cc
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-02-14 13:10:45 +0300

    Reorganize Yoksis endpoints

--- lib/support.rb
+++ lib/support.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 require_relative 'support/core_ext'
-require_relative 'support/detsis'
-require_relative 'support/mernis'
 require_relative 'support/rest_client'
 require_relative 'support/soap_client'
-require_relative 'support/yoksis'
+require_relative 'support/thirdparty'
---
+++ lib/support/thirdparty.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+require_relative 'thirdparty/detsis'
+require_relative 'thirdparty/mernis'
+require_relative 'thirdparty/yoksis'
