8ad3e63eb35904ee9de3712bc57d54f8c4597a44
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-02-15 14:45:57 +0300

    Start to think multi-tenancy

--- lib/support/service.rb
+++ lib/support/service.rb
@@ -3,3 +3,14 @@
 require_relative 'service/detsis'
 require_relative 'service/mernis'
 require_relative 'service/yoksis'
+
+module Service
+  CREDENTIALS = Rails.application.credentials.tenant[Tenant.active_tenant]
+
+  Yoksis.configure do |config|
+    config.username      = CREDENTIALS.yoksis[:username]
+    config.password      = CREDENTIALS.yoksis[:password]
+    config.client_id     = CREDENTIALS.yoksis[:client_id]
+    config.client_secret = CREDENTIALS.yoksis[:client_secret]
+  end
+end
--- lib/support/service/yoksis.rb
+++ lib/support/service/yoksis.rb
@@ -9,16 +9,8 @@ require_relative 'yoksis/unit'
 
 module Service
   module Yoksis
-    CREDENTIALS = Rails.application.credentials.yoksis
-
-    Reference.configure do |config|
-      config.endpoint = 'https://servisler.yok.gov.tr/ws/Referanslarv1?WSDL'
-    end
-
-    Staff.configure do |config|
-      config.endpoint = 'http://servisler.yok.gov.tr/ws/UniversiteAkademikPersonelv1?WSDL'
-      config.client_id = CREDENTIALS[:client_id]
-      config.client_secret = CREDENTIALS[:client_secret]
-    end
+    include ActiveSupport::Configurable
+    
+    config_accessor :username, :password, :client_id, :client_secret
   end
 end
