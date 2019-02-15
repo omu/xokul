09b595c2ba8435307425b19d1213452e9a8ac8ff
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-02-15 14:58:43 +0300

    .

--- lib/support/service.rb
+++ lib/support/service.rb
@@ -5,12 +5,10 @@ require_relative 'service/mernis'
 require_relative 'service/yoksis'
 
 module Service
-  CREDENTIALS = Rails.application.credentials.tenant[Tenant.active_tenant]
-
   Yoksis.configure do |config|
-    config.username      = CREDENTIALS.yoksis[:username]
-    config.password      = CREDENTIALS.yoksis[:password]
-    config.client_id     = CREDENTIALS.yoksis[:client_id]
-    config.client_secret = CREDENTIALS.yoksis[:client_secret]
+    config.username      = Tenant.credentials.yoksis[:username]
+    config.password      = Tenant.credentials.yoksis[:password]
+    config.client_id     = Tenant.credentials.yoksis[:client_id]
+    config.client_secret = Tenant.credentials.yoksis[:client_secret]
   end
 end
