d09edbd0aa999e859cf36afa2e26fade9ec8d08c
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-06-27 11:06:23 +0300

    Fix serialization error due to the wrong key

--- app/serializers/meksis/buildings_serializer.rb
+++ app/serializers/meksis/buildings_serializer.rb
@@ -3,7 +3,7 @@ module Meksis
   class BuildingsSerializer < Serializer
     attribute(:id)                    { integer object['BinaID']                         }
     attribute(:name)                  { string  object['BinaAdi']                        }
-    attribute(:short_name)            { integer object['BinaKisaAdi']                    }
+    attribute(:short_name)            { string  object['BinaKisaAdi']                    }
     attribute(:unit_id)               { integer object['AkademikIdariBirimID']           }
     attribute(:main_functionality_id) { integer object['AnaFonksiyonID']                 }
     attribute(:closed_area)           { float   object['KapaliAlan']                     }
