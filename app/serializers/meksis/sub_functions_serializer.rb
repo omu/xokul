b0713cb1244bf0252f48a50e9cce835f929428ef
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-07-03 11:57:33 +0300

    Minor change

--- app/serializers/meksis/sub_functions_serializer.rb
+++ app/serializers/meksis/sub_functions_serializer.rb
@@ -1,9 +1,9 @@
 
 module Meksis
   class SubFunctionsSerializer < Serializer
-    attribute(:id)   { string  object['AnaFonksiyonID']  }
-    attribute(:name) { string  object['AnaFonksiyonAdi'] }
-    attribute(:id)   { integer object['AltFonksiyonID']  }
-    attribute(:name) { string  object['AltFonksiyonAdi'] }
+    attribute(:parent_id)   { integer object['AnaFonksiyonID']  }
+    attribute(:parent_name) { string  object['AnaFonksiyonAdi'] }
+    attribute(:id)          { integer object['AltFonksiyonID']  }
+    attribute(:name)        { string  object['AltFonksiyonAdi'] }
   end
 end
