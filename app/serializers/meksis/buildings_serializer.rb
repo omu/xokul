30b8a7d7ac3a299f34ecc0b3d48fe8bbf4fe3d95
Hüseyin Tekinaslan <htkaslan@gmail.com>
2019-10-04 13:30:17 +0300

    Fix serializer key

--- app/serializers/meksis/buildings_serializer.rb
+++ app/serializers/meksis/buildings_serializer.rb
@@ -6,7 +6,7 @@ module Meksis
     attribute(:code)               { string  object['code']               }
     attribute(:unit_id)            { integer object['unit_id']            }
     attribute(:main_place_type_id) { integer object['main_place_type_id'] }
-    attribute(:interior_area)      { float   object['interior_id']        }
+    attribute(:indoor_area)        { float   object['indoor_area']        }
     attribute(:active)             { boolean object['active']             }
   end
 end
