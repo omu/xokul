fb897e3fff7af6c6caafe972dbc04d292b5893a4
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-03-03 10:48:10 +0300

    Set up almost sustainable monorepo implementation without too much effort

---
+++ client/lib/xokul/client.rb
@@ -0,0 +1,8 @@
+require "xokul/client/version"
+
+module Xokul
+  module Client
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ client/lib/xokul/client/version.rb
@@ -0,0 +1,5 @@
+module Xokul
+  module Client
+    VERSION = "0.1.0"
+  end
+end
