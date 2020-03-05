b91ecabf3769b09185a652ed0f8f472f3d41e9ef
Hüseyin Tekinaslan <htkaslan@gmail.com>
2020-03-05 11:35:13 +0300

    Add authentication module including necessary helpers

---
+++ client/lib/xokul/client/authentication.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+module Xokul
+  module Client
+    module Authentication
+      def basic_auth?
+        Configuration.user.present? && Configuration.password.present?
+      end
+
+      def bearer_auth?
+        Configuration.bearer_token.present?
+      end
+
+      def token_auth?
+        Configuration.access_token.present?
+      end
+
+      def authenticate
+        @http = http.basic_auth user: Configuration.user, pass: Configuration.password if basic_auth?
+        @http = http.auth "Bearer #{Configuration.bearer_token}" if bearer_auth?
+        @http = http.auth "token #{Configuration.access_token}" if token_auth?
+      end
+    end
+  end
+end
